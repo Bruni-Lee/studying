2339ddea08d605cb9c32de0cf6d6aecbbc95acaf
Bruni <dlgkstjd0534@naver.com>
2023-09-16 16:21:58 +0900

    update
    파일 위치변경, 싱글톤 복습 사용주기가 꽤나 있어 복습에 어려움이 없었음.

---
+++ C, C++, C# 등 - Studying/C#/디자인 패턴 복습/SingletonPattern.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+
+//싱글톤의 개념은 사용한 적이 많아 쉽게 복습했다.
+class SingletonPattern
+{
+    static void Main()
+    {
+
+        Singleton singleton1 = Singleton.Instance;
+        Singleton singleton2 = Singleton.Instance;
+
+        if (singleton1 == singleton2)
+            Console.WriteLine("they are same, singleton"); //실행시 이 코드 나옴
+        else
+            Console.WriteLine("they ar not same");
+    }
+    public class Singleton
+    {
+        static Singleton instance; 
+
+        public static Singleton Instance
+        {
+            get
+            {
+                if (null == instance)
+                {
+                   instance = new Singleton();
+                }
+                return instance;
+            }
+        }
+
+    }
+}
