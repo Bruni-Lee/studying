da66d6b0c217ee47367aad5230e12eabdc721cb8
Bruni <dlgkstjd0534@naver.com>
2023-09-17 12:44:34 +0900

    update
    옵저버 패턴 간단하게 복습. 직접 다시 써보고 만들고 하는게 까먹지 않는 부분에서도 도움이 되는 것 같다.

---
+++ C, C++, C# 등 - Studying/C#/디자인 패턴 복습/ObserverPattern.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+
+
+//옵저버 패턴 간단하게 복습!
+class ObserverPattern
+{
+
+    public interface I_Observe //관찰자 interface
+    {
+       public abstract void  Update();
+    }
+    static void Main()
+    {
+        Subject Owner = new Subject();
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+
+        Owner.Add_Observe(cat);
+        Owner.Add_Observe(dog);
+        Owner.Notify();
+    }
+    public class Subject //관찰 대상자
+    {
+        List<I_Observe> observers = new List<I_Observe>();
+       public void Add_Observe(I_Observe Observe)
+        {
+            observers.Add(Observe);
+        }
+       public void Remove_Observe(I_Observe Observe)
+        {
+            observers.Remove(Observe); 
+        }
+       public void Notify()
+        {
+            foreach (var Obj in observers)
+                Obj.Update();
+        }   
+    }
+    public class Cat : I_Observe // 관찰자
+    {
+        public void Update()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+    public class Dog : I_Observe //관찰자
+    {
+        public void Update()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+}
